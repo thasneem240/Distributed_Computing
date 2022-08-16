6ae323fefffe41ba3f63c4faa729af38c8446c28
thasneem240 <103405726+thasneem240@users.noreply.github.com>
2022-08-16 13:12:48 +0530

    App created by Curtin Lecturer

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> studentlist = StudentList.Students();
+           
+            foreach (Student student in studentlist)
+            {
+                Console.WriteLine("*******************************************************");
+                Console.WriteLine(student);
+            }
+            Console.WriteLine("***********************************************************");
+            Console.ReadLine();
+        }
+    }
+}
